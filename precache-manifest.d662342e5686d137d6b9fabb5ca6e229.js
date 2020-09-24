7d56216f000563d010a56769edbd1090d54176c8
Shubhankar Saxena <saxena03121999@gmail.com>
2020-09-24 13:01:00 +0530

    Updates

---
+++ precache-manifest.d662342e5686d137d6b9fabb5ca6e229.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7f45e4c99c014e79e066426b5b98464",
+    "url": "/Weather/index.html"
+  },
+  {
+    "revision": "9cac199a733e3f0d5a86",
+    "url": "/Weather/static/css/main.207c0fd9.chunk.css"
+  },
+  {
+    "revision": "7cf2182656a62ed6117a",
+    "url": "/Weather/static/js/2.d437b892.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather/static/js/2.d437b892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cac199a733e3f0d5a86",
+    "url": "/Weather/static/js/main.5d25e953.chunk.js"
+  },
+  {
+    "revision": "33b8acc232781a3d41c0",
+    "url": "/Weather/static/js/runtime-main.22be1482.js"
+  },
+  {
+    "revision": "8e4416f02aed6fd3a2b3b8b95beae9b0",
+    "url": "/Weather/static/media/cold-bg.8e4416f0.jpg"
+  },
+  {
+    "revision": "da3d207bd3b9636699446705c367bd27",
+    "url": "/Weather/static/media/warm-bg.da3d207b.jpeg"
+  }
+]);
